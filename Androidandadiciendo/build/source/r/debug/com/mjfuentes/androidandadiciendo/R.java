ae5957b1b3df07f3793cde3b2b24bfdf42e33bb2
Matias Fuentes <matiasj.fuentes@gmail.com>
2013-07-06 20:51:25 -0300

    last commit for today

--- Androidandadiciendo/build/source/r/debug/com/mjfuentes/androidandadiciendo/R.java
+++ Androidandadiciendo/build/source/r/debug/com/mjfuentes/androidandadiciendo/R.java
@@ -23,21 +23,22 @@ public final class R {
         public static final int utn_icon=0x7f020005;
     }
     public static final class id {
-        public static final int action_settings=0x7f08000e;
-        public static final int albumsList=0x7f080008;
+        public static final int action_settings=0x7f08000f;
+        public static final int adView=0x7f080002;
+        public static final int albumsList=0x7f080009;
         public static final int gallery=0x7f080000;
-        public static final int image=0x7f08000a;
+        public static final int image=0x7f08000b;
         public static final int index=0x7f080001;
-        public static final int listView=0x7f080005;
-        public static final int page_icon=0x7f08000d;
-        public static final int progressAlbums=0x7f080009;
-        public static final int progressBar=0x7f08000b;
-        public static final int progressImages=0x7f080004;
-        public static final int share=0x7f080003;
-        public static final int subTitle=0x7f08000c;
-        public static final int textView=0x7f080007;
-        public static final int textView2=0x7f080006;
-        public static final int title=0x7f080002;
+        public static final int listView=0x7f080006;
+        public static final int page_icon=0x7f08000e;
+        public static final int progressAlbums=0x7f08000a;
+        public static final int progressBar=0x7f08000c;
+        public static final int progressImages=0x7f080005;
+        public static final int share=0x7f080004;
+        public static final int subTitle=0x7f08000d;
+        public static final int textView=0x7f080008;
+        public static final int textView2=0x7f080007;
+        public static final int title=0x7f080003;
     }
     public static final class layout {
         public static final int activity_images=0x7f030000;
